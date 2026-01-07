cbb140074cfba202dbaeaa40cc68fbcdec5917b4
SooMin Chae <soomin.chae@sas.com>
2026-01-07 16:18:22 -0500

    For icons package

---
+++ frontend/src/radix-icons.d.ts
@@ -0,0 +1,6 @@
+declare module '@radix-ui/react-icons' {
+    import * as React from 'react';
+    export const MixerHorizontalIcon: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement>>;
+    export const ListBulletIcon: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement>>;
+    // Add any other icons you are using here as needed
+}
